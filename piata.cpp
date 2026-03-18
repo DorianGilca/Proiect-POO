fdfcf0af2ce6f0f7a2c354675d06988f2eed9e1a
DorianGilca <doriangdg1234@gmail.com>
2026-03-18 14:57:42 +0200

    Update piata.cpp

--- piata.cpp
+++ piata.cpp
@@ -4,19 +4,27 @@
 
 void Piata::citesteDate(const std::string& fisier) {
     std::ifstream in(fisier);
-    if (!in.is_open()) throw std::runtime_error("Nu se poate deschide fisierul " + fisier);
+    if (!in.is_open()) {
+        throw std::runtime_error("Eroare la deschiderea fisierului de piata: " + fisier);
+    }
 
     std::string linie;
     while (std::getline(in, linie)) {
-        if (linie.empty() || linie.find("Resursa") != std::string::npos) continue;
+        if (linie.empty() || linie.find("Resursa") != std::string::npos) {
+            continue;
+        }
 
         std::istringstream ss(linie);
         std::string token;
         Resursa res;
 
         std::getline(ss, res.nume, ';');
-        std::getline(ss, token, ';'); res.pret_min = std::stod(token);
-        std::getline(ss, token, ';'); res.pret_max = std::stod(token);
+        
+        std::getline(ss, token, ';'); 
+        res.pret_min = std::stod(token);
+        
+        std::getline(ss, token, ';'); 
+        res.pret_max = std::stod(token);
 
         resurse[res.nume] = res;
     }
@@ -24,8 +32,10 @@ void Piata::citesteDate(const std::string& fisier) {
 
 Resursa& Piata::getResursaRef(const std::string& nume) {
     auto it = resurse.find(nume);
-    if (it != resurse.end()) return it->second;
-    throw std::runtime_error("Resursa " + nume + " nu a fost gasita pe piata!");
+    if (it != resurse.end()) {
+        return it->second;
+    }
+    throw std::runtime_error("Eroare: Resursa " + nume + " nu a fost gasita!");
 }
 
 const std::map<std::string, Resursa>& Piata::getToateResursele() const {
