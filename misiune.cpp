c65c28af1bd86d48933bf1666f47101e8718c9f7
DorianGilca <doriangdg1234@gmail.com>
2026-03-18 14:55:33 +0200

    Update misiune.cpp

--- misiune.cpp
+++ misiune.cpp
@@ -1,11 +1,16 @@
 #include "misiune.h"
 #include <iomanip>
+#include <stdexcept>
 
 Misiune::Misiune(std::string idR, std::string idA, std::string res, double cant, double comb, double cost, double pBrut, double pNet)
     : idRacheta(idR), idAsteroid(idA), resursa(res), cantitateExtrasa(cant), combustibilConsum(comb), costCombustibil(cost), profitBrut(pBrut), profitNet(pNet) {}
 
 void Misiune::scrieMisiuni(const std::string& numeFisier, const std::vector<Misiune>& misiuni) {
     std::ofstream file(numeFisier);
+    if (!file.is_open()) {
+        throw std::runtime_error("Eroare la crearea fisierului de raport " + numeFisier);
+    }
+
     file << "ID Racheta;ID Asteroid;Resursa;Cantitate Extrasa;Combustibil Consumat;Cost Combustibil;Profit Brut;Profit Net\n";
 
     for (const auto& m : misiuni) {
